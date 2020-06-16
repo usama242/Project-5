f28279b4818dfe1c64261cdd81d572c5e914c550
Usama <mohamadusama242@gmail.com>
2020-06-16 05:00:25 +0200

    Feat: Initial Commit Setting up basic project structure and installing express

---
+++ src/server/index.js
@@ -0,0 +1,26 @@
+let path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static("dist"));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve("dist/index.html"));
+});
+const port = 8000;
+// designates what port the app will listen to for incoming requests
+app.listen(port, function () {
+  console.log(`The app is running on port ${port}!`);
+});
